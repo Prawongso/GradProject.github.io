78cb814361e0ecc5aa58c044e4a303681e635862
Prawongso <115227131+Prawongso@users.noreply.github.com>
2024-03-02 14:42:56 +0800

    FaceDetect
    FaceDetect non realtime version

--- Face-Mask-Detection-master/README.md
+++
@@ -1,6 +0,0 @@
-# Face-Mask-Detection
-Face Mask Detection With Deep Learning
-
-![Alt Text](https://i.giphy.com/media/Pj08zaSxNV2zBjPwer/giphy.webp)
-
-https://www.youtube.com/watch?v=rsuNcQQgYy0
--- FaceDetect/FaceDetec.py
+++ FaceDetect/FaceDetec.py
@@ -7,27 +7,21 @@ import cv2
 import os
 
 ap = argparse.ArgumentParser()
-ap.add_argument("-f", "--face", type=str,
-	default="face_detector",
-	help="path to face detector model directory")
-ap.add_argument("-m", "--model", type=str,
-	default="mask_detector.model",
-	help="path to trained face mask detector model")
-ap.add_argument("-c", "--confidence", type=float, default=0.5,
-	help="minimum probability to filter weak detections")
+ap.add_argument("-f", "--face", type=str, default="face_detector", help="path to face detector model directory")
+ap.add_argument("-m", "--model", type=str, default="mask_detector.model", help="path to trained face mask detector model")
+ap.add_argument("-c", "--confidence", type=float, default=0.5, help="minimum probability to filter weak detections")
 args = vars(ap.parse_args())
 
 
 print("loading face detector model")
 prototxtPath = os.path.sep.join([args['face'],"deploy.prototxt"])
-weightsPath = os.path.sep.join([args["face"],
-	"res10_300x300_ssd_iter_140000.caffemodel"])
+weightsPath = os.path.sep.join([args["face"],"res10_300x300_ssd_iter_140000.caffemodel"])
 net = cv2.dnn.readNet(prototxtPath, weightsPath)
 
 print("loading face mask detector model...")
 model = load_model("model.h5")
 
-image = cv2.imread("examples/image5.jpg")
+image = cv2.imread("input/face.jpg")
 orig = image.copy()
 (h, w) = image.shape[:2]
 
@@ -55,16 +49,16 @@ for i in range(0,detections.shape[2]):
         
         (mask, withoutMask) = model.predict(face)[0]
         
-        label = "Mask" if mask > withoutMask else "No Mask"
-        color = (0,255,0) if label == "Mask" else (0,0,255)
+        label = "Mask Detected" if mask > withoutMask else "No Mask Detected"
+        color = (0,255,0) if label == "Mask Detected" else (0,0,255)
     
-#         label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
-        
-#         cv2.putText(image, label, (w-110, h-10),
-# 			cv2.FONT_HERSHEY_SIMPLEX, 0.50, color, 2)
+        #label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
+        #cv2.putText(image, label, (w-110, h-10), cv2.FONT_HERSHEY_SIMPLEX, 0.50, color, 2)
+
         cv2.rectangle(image, (startX, startY), (endX, endY), color, 2)
+        cv2.putText(image, label, (startX+10,endY+25), cv2.FONT_HERSHEY_DUPLEX , 1, color)
 
 cv2.imshow("Output", image)
-
+cv2.waitKey(0)
 
 
